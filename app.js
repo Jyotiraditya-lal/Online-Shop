ecd7480ca238fe9e1fcddac384b9d76c065fcd98
Jyotiraditya <132455858+Jyotiraditya-lal@users.noreply.github.com>
2023-09-10 12:23:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const admin=require('./Routes/admin')
 const shop=require('./Routes/shop')
 const path=require('path')
 const contactRoute=require('./Routes/contact-us')
+const errorController=require('./Controllers/error')
 
 const app=express()
 
@@ -14,8 +15,6 @@ app.use(shop)
 app.use(contactRoute)
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use((req,res,next)=>{
-    res.status(404).sendFile(path.join(__dirname, 'Views', 'Error.html'))
-})
+app.use(errorController.error)
 
 app.listen(3000)
