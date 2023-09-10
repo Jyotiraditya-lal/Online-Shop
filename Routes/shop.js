8136b0aefc891a8ff390510f41df22ea72c70911
Jyotiraditya <132455858+Jyotiraditya-lal@users.noreply.github.com>
2023-09-10 12:21:51 +0530

    Update shop.js

--- Routes/shop.js
+++ Routes/shop.js
@@ -1,11 +1,9 @@
-const path=require('path')
+
 const express=require('express')
-const rootDir= require('../Util/path')
+const productController=require('../Controllers/products')
 
 const router=express.Router()
 
-router.get('/',(req,res,next)=>{
-    res.sendFile(path.join(rootDir, 'Views', 'shop.html'))
-})
+router.get('/',productController.getShop)
 
 module.exports= router
